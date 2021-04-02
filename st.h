7a8a24d9a5a5516ee9f60c2a3b1ca1c3818782a6
Fululuhu <fululuhu@fArch.localdomain>
2021-04-03 00:09:47 +0200

    apply alpha

--- st.h
+++ st.h
@@ -134,4 +134,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern float alpha;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
