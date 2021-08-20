857adcdb7e18795780094bfa0246655b8d524846
tpmls <tpmls@email.cz>
2021-08-20 19:42:57 +0200

    Apply scrollback-mouse-increment

--- config.def.h
+++ config.def.h
@@ -163,8 +163,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 120;
+static unsigned int rows = 32;
 
 /*
  * Default colour and shape of the mouse cursor
@@ -226,10 +226,11 @@ ResourcePref resources[] = {
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
+const unsigned int mousescrollincrement = 1;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
+    { ShiftMask,            Button4, kscrollup,      {.i =  mousescrollincrement} },
+    { ShiftMask,            Button5, kscrolldown,    {.i =  mousescrollincrement} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
