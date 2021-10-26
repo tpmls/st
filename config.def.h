f4dc15ae152cc9ef970bd122f54e39c035801740
tpmls <tpmls@email.cz>
2021-10-26 11:27:16 +0200

    Fix colors

--- config.def.h
+++ config.def.h
@@ -134,10 +134,10 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#bfbfbf", // 256 - cursor
-	"#555555", // 257 - reverse cursor
-    "#bfbfbf", // 258 - foreground
-    "#000000", // 259 - background
+    "#000000", // 256 - background
+    "#bfbfbf", // 257 - foreground
+	"#bfbfbf", // 258 - cursor
+	"#555555", // 259 - reverse cursor
 };
 
 
@@ -145,10 +145,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
